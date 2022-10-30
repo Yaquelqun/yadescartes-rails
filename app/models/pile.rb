ac99bd427316157939b1f98c092a0b747ae1b87f
Yaquelqun <sandjiv.parassou@gmail.com>
2022-10-30 15:40:36 +0100

    add other pendant of the polymorphic

--- app/models/pile.rb
+++ app/models/pile.rb
@@ -1,3 +1,4 @@
 class Pile < ApplicationRecord
   belongs_to :lobby
+  has_many :cards, as: :owner
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :past_participations, -> { inactive }, class_name: 'Participation', dependent: :destroy
   has_many :past_lobbies, through: :past_participations, class_name: 'Lobby', source: :lobby
 
+  has_many :cards, as: :owner
   def busy?
     participation.present?
   end
