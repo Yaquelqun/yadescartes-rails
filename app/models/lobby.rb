f94cb288788162b519f551541b6af6fe7def6940
Yaquelqun <sandjiv.parassou@gmail.com>
2022-10-30 12:23:21 +0100

    add validation on lobby and some dependency on destruction

--- app/models/lobby.rb
+++ app/models/lobby.rb
@@ -1,4 +1,11 @@
 class Lobby < ApplicationRecord
-  has_many :participations
+  has_many :participations, dependent: :destroy
   has_many :users, through: :participations
+
+  WAITING_FOR_PLAYERS = 'waiting_for_players'.freeze
+  ONGOING = 'ongoing'.freeze
+  FINISHED = 'finished'.freeze
+  STATUSES = [WAITING_FOR_PLAYERS, ONGOING, FINISHED].freeze
+
+  validates :status, inclusion: { in: STATUSES }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_one :participation, -> { active }
+  has_one :participation, -> { active }, dependent: :destroy
   has_one :lobby, through: :participation
 
-  has_many :past_participations, -> { inactive }, class_name: 'Participation'
+  has_many :past_participations, -> { inactive }, class_name: 'Participation', dependent: :destroy
   has_many :past_lobbies, through: :past_participations, class_name: 'Lobby', source: :lobby
 end
