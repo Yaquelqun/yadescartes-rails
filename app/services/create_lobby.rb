dd261fd8dc133152ce563f4138dca66982f48e5c
Yaquelqun <sandjiv.parassou@gmail.com>
2022-10-30 15:05:42 +0100

    change create lobby to only generate a lobby instead of subscribing users

--- app/services/create_lobby.rb
+++ app/services/create_lobby.rb
@@ -1,38 +1,13 @@
 class CreateLobby
-  attr_reader :user_id
-
-  def initialize(user_id: nil)
-    @user_id = user_id
-  end
-
   def call
-    check_user_availability
     lobby = create_lobby
-    add_user_to(lobby)
 
     lobby
   end
 
   private
-
-  def check_user_availability
-    return unless user_id && user.busy?
-
-    raise Errors::ApplicationError.new(status: :unprocessable_entity, message: 'user_already_in_lobby')
-  end
-
   def create_lobby
     Lobby.create!(status: Lobby::WAITING_FOR_PLAYERS)
-  end
-
-  def add_user_to(lobby)
-    return unless user_id
-
-    lobby.users << user
-  end
-
-  def user
-    @user ||= User.find(user_id)
   end
 end
 
--- spec/services/create_lobby_spec.rb
+++ spec/services/create_lobby_spec.rb
@@ -3,54 +3,16 @@ require 'rails_helper'
 describe CreateLobby do
   describe 'call' do
     subject(:service_response) do
-      described_class.new(**service_arguments).call
+      described_class.new.call
     rescue Errors::ApplicationError => exception
       exception
     end
 
     let(:user) { create(:user) }
 
-    context 'when given a free user' do
-      let(:service_arguments) { { user_id: user.id } }
-
-      it 'does not raise an error' do
-        expect { described_class.new(**service_arguments).call }.not_to raise_error
-      end
-
-      it 'creates a lobby and returns it' do
-        expect(service_response.class).to eql Lobby
-      end
-
-      it 'adds the user as a participant in the lobby' do
-        expect(service_response.users.ids).to eql [user.id]
-      end
-    end
-
-    context 'without arguments' do
-      let(:service_arguments) { { user_id: nil } }
-
-      it 'creates an empty lobby' do
-        expect(service_response.class).to eql(Lobby)
-        expect(service_response.users).to be_empty
-      end
-    end
-
-    context 'when given a busy user' do
-      let(:lobby) { create(:lobby, users: [user]) }
-      let(:service_arguments) { { user_id: user.id } }
-
-      before do
-        lobby
-      end
-
-      it 'raises an application error' do
-        expect(service_response.class).to eql(Errors::ApplicationError)
-        expect(service_response.message).to eql('user_already_in_lobby')
-      end
-
-      it 'does not create a lobby' do
-        expect { service_response }.not_to change(Lobby, :count)
-      end
+    it 'creates an empty lobby' do
+      expect(service_response.class).to eql(Lobby)
+      expect(service_response.users).to be_empty
     end
   end
 end
